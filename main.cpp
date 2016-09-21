1266a8bc16788aaa4ac9aee258fb6e5097ac2b9e
Mike Ellertson <mdellertson@gmail.com>
2016-09-21 02:43:29 -0700

    Added additional comments. Updated code to output all non-error messages to stdout. Updated code to output all errors to stderr.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ const char *admin_dn      = "cn=read-only-admin,dc=example,dc=com";
 char *admin_pw            = strdup("password");
 char *user_dn             = NULL;
 char *user_pw             = strdup("password");
+char *user_id             = strdup("uid=einstein");
+
 char ldap_uri[255];
 char *ldapErrorString = NULL;
 berval credentials = { strlen(admin_pw), admin_pw }; // Admin credentials on LDAP server
@@ -38,44 +40,46 @@ int main() {
 	int status = ldap_initialize(&ld, ldap_uri);
 	if (status != LDAP_SUCCESS) {
 		// Output the error message
-		perror("ldap_initialize() failed");
 		ldapErrorString = ldap_err2string(status);
-		printf("Connection to server failed with [%s].", ldapErrorString);
-		printf("LDAP server initialization error. Check [%s] server's status.", ldap_uri);
+		fprintf(stderr, "Connection to server failed with [%s].", ldapErrorString);
+		fprintf(stderr, "LDAP server initialization error. Check [%s] server's status.", ldap_uri);
 	}
-	printf("Successfully connected to %s\n", ldap_uri);
+	fprintf(stdout, "Successfully connected to %s\n", ldap_uri);
 
 	// *****************************************
 	// Login using the admin's credentials
 	return_code = ldap_sasl_bind_s( ld, admin_dn, NULL, &credentials, NULL, NULL, &returned_cred );
 	if( return_code != LDAP_SUCCESS )
 	{
-		fprintf(stderr, "ldap_sasl_bind returned: %s\n", ldap_err2string(return_code) );
+		// Output the error message
+		fprintf(stderr, "The admin username and password were invalid: %s\n", ldap_err2string(return_code) );
 		return( return_code );
 	}
 	ber_memfree(returned_cred);
 
 	// *****************************************************
 	// Search for the user's record on the LDAP server
-	return_code = ldap_search_ext_s(ld, base_dn, LDAP_SCOPE_SUBTREE, "uid=einstein", NULL, 0, NULL, NULL, NULL, 0, &records);
+	return_code = ldap_search_ext_s(ld, base_dn, LDAP_SCOPE_SUBTREE, user_id, NULL, 0, NULL, NULL, NULL, 0, &records);
 	if ( return_code != LDAP_SUCCESS ) {
-		fprintf(stderr, "ldap_search_ext_s: %s\n", ldap_err2string(return_code));
+		// Output the error message
+		fprintf(stderr, "The username is invalid: %s\n", ldap_err2string(return_code));
 	}
 
-	// *************************************
-	// Validate the user's password
+	// ******************************************
+	// Validate the user's login and password
 	for ( record = ldap_first_entry( ld, records ); record != NULL; record = ldap_next_entry( ld, record ) ) {
 		if ( (user_dn = ldap_get_dn( ld, record )) != NULL ) {
-			printf( "Retrieved this record from the LDAP server: %s\n", user_dn );
-			/* rebind */
+			fprintf(stdout, "Retrieved this record from the LDAP server: %s\n", user_dn );
+			// ********************************************************************************
+			// rebind using the user's distinguished name (user_dn) and password (user_cred)
 			ldap_initialize(&ld2, ldap_uri);
 			return_code = ldap_sasl_bind_s(ld2, user_dn, NULL, &user_cred, NULL, NULL, &returned_cred);
-			printf("Return code from LDAP server after ldap_sasl_bind_s(): %d\n", return_code);
 			if (return_code != 0) {
-				printf("The user credentials couldn't be authenticated.\n");
+				// Output the error message
+				fprintf(stderr, "The user's password was invalid.\n");
 				return (return_code);
 			} else {
-				printf("The user was authenticated.\n");
+				fprintf(stdout, "The user was authenticated.\n");
 				ldap_unbind_ext(ld2, NULL, NULL);
 			}
 			ber_memfree(returned_cred);
